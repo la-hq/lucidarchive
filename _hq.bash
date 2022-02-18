970f284722611efccba1824732ccac36a14f7656
HQ <hq@lucidarchive.com>
2022-02-17 18:21:15 -0800

    FILTER_BRANCH_SQUELCH_WARNING=1 in _hq.bash

--- _hq.bash
+++ _hq.bash
@@ -5,7 +5,8 @@
     T=timestamp-$(date +%s)
     R=$(git stash save $t)
 
-    git filter-branch -f --env-filter "GIT_AUTHOR_NAME='HQ'; GIT_AUTHOR_EMAIL='hq@lucidarchive.com'; GIT_COMMITTER_NAME='HQ'; GIT_COMMITTER_EMAIL='hq@lucidarchive.com';" $SHA..HEAD
+    FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f \
+        --env-filter "GIT_AUTHOR_NAME='HQ'; GIT_AUTHOR_EMAIL='hq@lucidarchive.com'; GIT_COMMITTER_NAME='HQ'; GIT_COMMITTER_EMAIL='hq@lucidarchive.com';" $SHA..HEAD
 
     V=$(echo $R | grep $T)
     if [ "$V" ]; then
