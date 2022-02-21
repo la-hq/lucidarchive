d9ad053daec692c6543d329be0607a10ae3cece0
HQ <hq@lucidarchive.com>
2022-02-21 10:04:31 -0800

    Re-add _hq.bash

---
+++ _hq.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+{
+    SHA="$(git rev-parse --short head~5|tr -d '\n')"
+
+    T=timestamp-$(date +%s)
+    R=$(git stash save $t)
+
+    FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f \
+        --env-filter "GIT_AUTHOR_NAME='HQ'; GIT_AUTHOR_EMAIL='hq@lucidarchive.com'; GIT_COMMITTER_NAME='HQ'; GIT_COMMITTER_EMAIL='hq@lucidarchive.com';" $SHA..HEAD
+
+    V=$(echo $R | grep $T)
+    if [ "$V" ]; then
+        git stash pop
+    fi
+}
